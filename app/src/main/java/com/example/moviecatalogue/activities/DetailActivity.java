b8461b7d9481afa9f02c3f523a6033d899729b0d
aldiwildan77 <aldiwild77@gmail.com>
2019-11-26 08:02:53 +0700

    sync and adding detail activity

--- app/src/main/java/com/example/moviecatalogue/activities/DetailActivity.java
+++ app/src/main/java/com/example/moviecatalogue/activities/DetailActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.moviecatalogue.R;
+
 public class DetailActivity extends AppCompatActivity {
 
     @Override
