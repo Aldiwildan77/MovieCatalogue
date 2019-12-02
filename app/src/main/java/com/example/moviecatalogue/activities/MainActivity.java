fc879c3f67bb14b8247f47a6b3f94a0f1992becf
aldiwildan77 <aldiwild77@gmail.com>
2019-12-03 01:53:10 +0700

    notification staging

--- app/src/main/java/com/example/moviecatalogue/activities/MainActivity.java
+++ app/src/main/java/com/example/moviecatalogue/activities/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.moviecatalogue.activities;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -13,6 +18,10 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.moviecatalogue.R;
 import com.example.moviecatalogue.fragments.MovieFragment;
 import com.example.moviecatalogue.fragments.SettingFragment;
+import com.example.moviecatalogue.services.NotificationReceiver;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +29,17 @@ public class MainActivity extends AppCompatActivity {
     FragmentTransaction fragmentTransaction;
     FragmentTransaction settingFragmentTransaction;
 
-
+    SharedPreferences mPrefences;
+    boolean isReminderOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPrefences = getSharedPreferences(getApplication().toString(), MODE_PRIVATE);
+        isReminderOn = mPrefences.getBoolean(SettingFragment.getKeyReminder(), SettingFragment.isDefaultReminder());
+        if (isReminderOn) setupNotification();
 
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
@@ -33,9 +47,7 @@ public class MainActivity extends AppCompatActivity {
         MovieFragment movieFragment = new MovieFragment();
         fragmentTransaction.replace(R.id.frame_test, movieFragment);
         fragmentTransaction.commit();
-
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.menu_item_setting){
+        if (item.getItemId() == R.id.menu_item_setting) {
             settingFragmentTransaction = fragmentManager.beginTransaction();
             SettingFragment settingFragment = new SettingFragment();
 
@@ -59,6 +71,20 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void setupNotification() {
+        Intent notificationIntent = new Intent(this, NotificationReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
+        Calendar updateTime = Calendar.getInstance();
+        updateTime.setTimeInMillis(System.currentTimeMillis());
+        updateTime.set(Calendar.HOUR_OF_DAY, 7);
+        updateTime.set(Calendar.MINUTE, 0);
 
+        int offset = updateTime.getTimeZone().getOffset(updateTime.getTimeInMillis());
+        long futureInMillis = updateTime.getTimeInMillis() + offset;
+        System.out.println(futureInMillis + " " + updateTime.getTimeInMillis());
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, futureInMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
+    }
 }
--- app/src/main/java/com/example/moviecatalogue/services/NotificationReceiver.java
+++ app/src/main/java/com/example/moviecatalogue/services/NotificationReceiver.java
@@ -1,24 +1,49 @@
 package com.example.moviecatalogue.services;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.TaskStackBuilder;
+
+import com.example.moviecatalogue.R;
 
 public class NotificationReceiver extends BroadcastReceiver {
 
+    public static String CHANNEL_ID = "channel-01";
+    public static String CHANNEL_NAME= "Channel Name";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("Notif Receiver", "Execute: on receive");
-        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            createNotification(context);
-        } else {
-            Toast.makeText(context.getApplicationContext(), "Alarm Manager just ran", Toast.LENGTH_LONG).show();
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        int notificationId = 1;
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(
+                    CHANNEL_ID, CHANNEL_NAME, importance);
+            notificationManager.createNotificationChannel(mChannel);
         }
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Movie Catalogue")
+                .setContentText("New Movie Update!");
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addNextIntent(intent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        notificationManager.notify(notificationId, mBuilder.build());
     }
 
-    private void createNotification(Context context){
-        NotificationTrigger.startNotification(context);
-    }
 }
